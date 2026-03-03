97b532d95b5c087746f5f8831289464765cc49c7
xcccc <azimamikadir@gmail.com>
2026-03-03 11:47:30 +0800

    chap 1

---
+++ 01/01_variables_data_type.py
@@ -0,0 +1,8 @@
+name = 'bob'  # string
+age = 10  # Integer
+weight = 20.5  # Float
+is_female = False  # Boolean
+random_number = None  # NoneType
+
+print(name)
+print(type(random_number))
---
+++ 01/01a_Math_Operations.py
@@ -0,0 +1,10 @@
+x = 10
+y = 3
+
+print(x+y)  # Addition
+print(x-y)  # Subtraction
+print(x*y)  # Multiplication
+print(x/y)  # Division
+print(x//y)  # edivide with no float(takde balance)
+print(x % y)  # divide than take balance left
+print(x**y)  # Power of

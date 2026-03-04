da3307503618ffb2d9011632e413f5715231c29c
xcccc <azimamikadir@gmail.com>
2026-03-04 11:13:22 +0800

    Chap 8

---
+++ 08/08a_curly.py
@@ -0,0 +1,17 @@
+fruits = {"apple", "banana", "orange"}
+
+#operations
+fruits.add("grape")
+fruits.remove("banana")
+fruits.discard("kiwi")
+
+
+print(fruits)
+
+#math operatios
+set1 ={1,2,3,4}
+set2 = {3,4,5,6}
+
+print(set1.union(set2))
+print(set1.intersection(set2))
+print(set1.difference(set2))

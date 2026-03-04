ee3cc9d7588394168a3e251b365871ab91fc019d
xcccc <azimamikadir@gmail.com>
2026-03-04 11:12:48 +0800

    chap 7

---
+++ 07/07_tuples.py
@@ -0,0 +1,7 @@
+#same like dataset, but have a lot of data in one dataset
+coordinates = (10,20)
+person = ("alice", 25, "Engineer")
+single_item = (42,)
+
+print(coordinates[0])
+print(len(person))
